86b7d89c23fd33d8d8313a9df4f75bfb6e06ff06
Emmanuel-okello-awany <2300811768@std.kyu.ac.ug>
2025-03-19 20:00:11 +0300

    Registered the user model in the admin panel

--- VideoLibrary/users/admin.py
+++ VideoLibrary/users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser  # Import your custom user model
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Information', {'fields': ('username', 'profile_picture')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'profile_picture', 'password1', 'password2', 'is_active', 'is_staff'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
